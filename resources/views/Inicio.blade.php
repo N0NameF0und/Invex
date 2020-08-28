e6ff9ce22fad68099d6b0e1d958633407bb50071
N0NameF0und@github.com <kam.zapata@yavirac.edu.ec>
2020-08-28 13:56:26 -0500

    update

---
+++ resources/views/Inicio.blade.php
@@ -0,0 +1,142 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="{{asset('static/css/home.css')}}">
+  <link rel="stylesheet" href="{{asset('static/css/font.css')}}">
+  <link rel="stylesheet" href="{{asset('static/css/main.css')}}">
+  <title>Home</title>
+</head>
+
+<body>
+  <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+    <span class="navbar-brand mb-0 h1">InveNX1</span>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="{{url('inicio')}}">Inicio<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{url('contactos')}}">Informacion</a>
+        </li>
+
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <a class="btn btn-outline-success my-2 my-sm-0" href="{{url('login')}}">Login</a>
+      </form>
+    </div>
+  </nav>
+  <div class="social-bar">
+    <a href="" class="icon icon-facebook" target="_blank"></a>
+    <a href="" class="icon icon-twitter" target="_blank"></a>
+    <a href="" class="icon icon-youtube" target="_blank"></a>
+    <a href="" class="icon icon-instagram" target="_blank"></a>
+  </div>
+  <main class='mainContainer'>
+    <div>
+      <div class="container">
+        <h1 class="text-center"></h1>
+        <br></br>
+        <br></br><br></br <!-- image left -->
+        <div class="row">
+          <div class="col-md-6">
+
+            <img src="https://static.wixstatic.com/media/1f2b93_8afcb6703aa648db8af8c1367163b093~mv2.jpg/v1/fill/w_584,h_389,al_c,q_90,usm_0.66_1.00_0.01/1f2b93_8afcb6703aa648db8af8c1367163b093~mv2.webp" alt="" class="w-100" />
+          </div>
+          <!-- /.col-md-6 -->
+          <div class="col-md-6">
+
+            <div class="row align-items-center h-100">
+              <div class="col" style="background: #F0480D ;">
+                <h1 class="display-3">InveNX1</h1>
+                <br /><br />
+                <p class="lead">Organización y Control
+
+                </p>
+
+              </div>
+            </div>
+
+          </div>
+          <!-- /.col-md-6 -->
+         
+        </div>
+        
+        <!-- /.row -->
+<br></br>
+        
+
+        <div class="text-center">
+            <h1 class="display-5">¿Qué es?</h1>
+           
+            <p class="lead"> Una herramientas  de 
+            gestión de inventario que proporciona
+             la funcionalidad para <strong>almacenar</strong>, realizar un<strong> seguimiento</strong> y cumplir los pedidos en 
+             función de la información de inventario en tiempo real de la planta, el almacén y las
+              ubicaciones subcontratadas y de proveedores .el seguimiento, la<strong> gestión</strong> y el control 
+               del inventario y los costos asociados de una manera sencilla</p>
+          </div>
+      
+
+      </div>
+
+
+
+
+      <section class="border-top">
+        <div class="container">
+          <div class="row text-center py-5">
+            <div class="col col-12 col-sm-12 col-md-6 col-lg-4 p-5 border-bottom border-sm-bottom border-md-bottom-0 border-md-right">
+              <svg width="4em" height="4em" viewBox="0 0 16 16" class="bi bi-phone" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M11 1H5a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z" />
+                <path fill-rule="evenodd" d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+              </svg>
+              <h2 class="h3 mt-3 mb-5 text-muted">Uso en dispositivos moviles</h2>
+              <p>De forma predeterminada, prácticamente todos los teléfonos móviles muestran automáticamente, 
+                y en la medida de lo posible, una versión móvil de las páginas web . permitiendo tener acceso al mismo contenido 
+                 en la versión móvil de un sitio web, equivalente al sitio web original .</p>
+            </div>
+            <div class="col col-12 col-sm-12 col-md-6 col-lg-4 p-5 border-bottom border-sm-bottom border-md-bottom-0 border-lg-right">
+              <svg width="4em" height="4em" viewBox="0 0 16 16" class="bi bi-bezier" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M0 10.5A1.5 1.5 0 0 1 1.5 9h1A1.5 1.5 0 0 1 4 10.5v1A1.5 1.5 0 0 1 2.5 13h-1A1.5 1.5 0 0 1 0 11.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zm10.5.5A1.5 1.5 0 0 1 13.5 9h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1a1.5 1.5 0 0 1-1.5-1.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zM6 4.5A1.5 1.5 0 0 1 7.5 3h1A1.5 1.5 0 0 1 10 4.5v1A1.5 1.5 0 0 1 8.5 7h-1A1.5 1.5 0 0 1 6 5.5v-1zM7.5 4a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1z" />
+                <path d="M6 4.5H1.866a1 1 0 1 0 0 1h2.668A6.517 6.517 0 0 0 1.814 9H2.5c.123 0 .244.015.358.043a5.517 5.517 0 0 1 3.185-3.185A1.503 1.503 0 0 1 6 5.5v-1zm3.957 1.358A1.5 1.5 0 0 0 10 5.5v-1h4.134a1 1 0 1 1 0 1h-2.668a6.517 6.517 0 0 1 2.72 3.5H13.5c-.123 0-.243.015-.358.043a5.517 5.517 0 0 0-3.185-3.185z" />
+              </svg>
+              <h2 class="h3 mt-3 mb-5 text-muted">Interconectado</h2>
+              <p>Con una instancia en SQL Server con garantías con respecto a la ubicación de las bases de datos 
+                que permite conectar un mundo de máquinas virtuales o locales.</p>
+            </div>
+            <div class="col col-12 col-sm-12 col-md-12 col-lg-4 p-5 m-auto border-md-top border-lg-top-0">
+              <svg width="4em" height="4em" viewBox="0 0 16 16" class="bi bi-tv" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M2.5 13.5A.5.5 0 0 1 3 13h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zM13.991 3H2c-.325 0-.502.078-.602.145a.758.758 0 0 0-.254.302A1.46 1.46 0 0 0 1 4.01V10c0 .325.078.502.145.602.07.105.17.188.302.254a1.464 1.464 0 0 0 .538.143L2.01 11H14c.325 0 .502-.078.602-.145a.758.758 0 0 0 .254-.302 1.464 1.464 0 0 0 .143-.538L15 9.99V4c0-.325-.078-.502-.145-.602a.757.757 0 0 0-.302-.254A1.46 1.46 0 0 0 13.99 3zM14 2H2C0 2 0 4 0 4v6c0 2 2 2 2 2h12c2 0 2-2 2-2V4c0-2-2-2-2-2z" />
+              </svg>
+              <h2 class="h3 mt-3 mb-5 text-muted">Uso en escritorio</h2>
+              <p> Para una empresa las computadoras son un activo esencial y una herramienta fundamental 
+                sin la cual sería casi imposible realizar cualquier tarea cotidiana en una empresa, por lo cual
+                esta implementada para que el diseño se adapte y pueda aprovechar todas las capacidades del uso de una 
+                computadora de escritorio .</p>
+            </div>
+          </div>
+        </div>
+      </section>
+  </main>
+  <footer>
+    <div class="contenedor">
+      <p class="copy">InveNX1 &copy; 2020</p>
+    </div>
+  </footer>
+</body>
+
+</html>
